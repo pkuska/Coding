e9aff9074a3192d62eacbd0f5c0d8cf0658f1efa
pku <pku@netcompany.com>
2017-01-23 11:46:51 +0100

    NUnit added

---
+++ SPOJ/SPOJ/Problem002.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SPOJ
+{
+    public class Problem002
+    {
+        public static void Main()
+        {
+            int n;
+            while ((n = int.Parse(Console.ReadLine())) != 42)
+                Console.WriteLine(n);
+        }
+    }
+}
---
+++ SPOJ/SPOJ/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SPOJ
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Problem002.Main();
+        }
+    }
+}
