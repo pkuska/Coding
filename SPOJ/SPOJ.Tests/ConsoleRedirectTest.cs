2cc199b23f701d6f0bc858675e195668340dd5a7
pku <pku@netcompany.com>
2017-01-31 11:47:28 +0100

    Further refinements to tests and Problem 2 solution

---
+++ SPOJ/SPOJ.Tests/ConsoleRedirectTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace SPOJ.Tests
+{
+    public class ConsoleRedirectFixture
+    {
+        private readonly Action _testAction;
+        public const string Separator = " ";
+
+        public ConsoleRedirectFixture(Action testAction)
+        {
+            _testAction = testAction;
+        }
+
+        protected void InputOutputTest(int[] input, int[] expectedOutput)
+            =>
+                InputOutputTest(input.Select(x => x.ToString()).ToArray(),
+                    expectedOutput.Select(x => x.ToString()).ToArray());
+
+        protected void InputOutputTest(int[][] input, int[][] expectedOutput)
+            =>
+        InputOutputTest(input.Select(x => x.Select(y => y.ToString()).ToArray()).ToArray(),
+            expectedOutput.Select(x => x.Select(y => y.ToString()).ToArray()).ToArray());
+
+        protected void InputOutputTest(string[][] input, string[][] expectedOutput)
+            =>
+                InputOutputTest(input.Select(x => string.Join(Separator, x)).ToArray(),
+                    expectedOutput.Select(x => string.Join(Separator, x)).ToArray());
+
+        protected void InputOutputTest(string[] input, string[] expectedOutput)
+            =>
+                InputOutputTest(string.Join(Environment.NewLine, input),
+                    string.Join(Environment.NewLine, expectedOutput));
+
+        protected void InputOutputTest(string input, string expectedOutput)
+        {
+            using (var reader = new StringReader(input))
+            using (var writer = new StringWriter())
+            {
+                Console.SetIn(reader);
+                Console.SetOut(writer);
+                _testAction();
+                Assert.That(expectedOutput.Trim(), Is.EqualTo(writer.ToString().Trim()));
+            }
+        }
+    }
+}
