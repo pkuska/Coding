858496d4b423730a4e9a45c5d75db25663a7524b
pku <pku@netcompany.com>
2017-02-01 13:27:51 +0100

    Problem 42

---
+++ SPOJ/SPOJ.Tests/Problem042Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace SPOJ.Tests
+{
+    [TestFixture]
+    public class Problem042Test : ConsoleRedirectFixture
+    {
+        public Problem042Test() : base(Problem042.Main)
+        {
+        }
+
+        [TestCase(new object[] { 1, new[] { 5, 3 }, 1, 2, 8, 4, 9 }, 3)]
+        public void MainTest(object[] input, int expectedOutput)
+        {
+            InputOutputTest(input, expectedOutput);
+        }
+    }
+}
---
+++ SPOJ/SPOJ/Problem042.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SPOJ
+{
+    public class Problem042
+    {
+        public static void Main()
+        {
+            for (var t = int.Parse(Console.ReadLine()); t > 0; --t)
+            {
+                var inputLine = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+                var n = inputLine[0];
+                var c = inputLine[1];
+                var stalls = new List<int>();
+                for (var j = 0; j < n; ++j)
+                    stalls.Add(int.Parse(Console.ReadLine()));
+                stalls.Sort();
+                int b = 0, max = 0, e = stalls.Last() - stalls.First();
+                while (b < e)
+                {
+                    var mid = (e + b) / 2;
+                    if (PlaceCows(c, mid, stalls))
+                    {
+                        max = Math.Max(max, mid);
+                        b = mid + 1;
+                    }
+                    else
+                        e = mid;
+                }
+                Console.WriteLine(max.ToString());
+            }
+        }
+
+        private static bool PlaceCows(int c, int s, IReadOnlyList<int> stalls)
+        {
+            int p = stalls[0], cows = 1;
+            for (var i = 1; i < stalls.Count; i++)
+            {
+                if (stalls[i] - p < s) continue;
+                p = stalls[i];
+                cows++;
+                if (cows == c) return true;
+            }
+            return false;
+        }
+    }
+}
