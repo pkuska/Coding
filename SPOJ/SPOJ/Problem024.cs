800b48190c2d59cd6818adb2840354569a2b34f3
pku <pku@netcompany.com>
2017-02-01 22:06:27 +0100

    Problem 024

---
+++ SPOJ/SPOJ/Problem024.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace SPOJ
+{
+    public class Problem024
+    {
+        public static void Main()
+        {
+            for (var t = int.Parse(Console.ReadLine()); t > 0; --t)
+                Console.WriteLine(Factorial(int.Parse(Console.ReadLine())).ToString());
+        }
+
+        private static BigInteger Factorial(int n)
+        {
+            if (n == 0) return 1;
+            return Factorial(n - 1) * n;
+        }
+    }
+}
